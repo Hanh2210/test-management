4367920d1121ae313a2b8dc5d320f2006e74e235
Hanh Nguyen <nguyen.thi.hong.hanh@azoom.jp>
2023-06-22 11:12:43 +0700

    change style for login, register

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,15 +1,52 @@
 <script lang="ts" setup>
+import hustImage1 from "@/assets/images/hust1.png";
+import hustImage2 from "@/assets/images/hust2.png";
+import hustImage3 from "@/assets/images/hust3.png";
+import hustImage4 from "@/assets/images/hust4.png";
+
 const pageContainer = ref<HTMLElement>();
 
 const nuxtApp = useNuxtApp();
 nuxtApp.hook("page:finish", () => {
   pageContainer.value?.scroll(0, 0);
 });
+const images = [
+  {
+    src: hustImage1,
+  },
+  {
+    src: hustImage2,
+  },
+  {
+    src: hustImage3,
+  },
+  {
+    src: hustImage4,
+  },
+];
 </script>
 <template>
   <div class="app-layout">
     <div ref="pageContainer" class="page-container">
-      <slot />
+      <div class="slider-show">
+        <v-carousel
+          cycle
+          :height="'100%'"
+          hide-delimiter-background
+          show-arrows="hover"
+        >
+          <v-carousel-item
+            v-for="(image, i) in images"
+            :key="i"
+            :src="image.src"
+            cover
+          >
+          </v-carousel-item>
+        </v-carousel>
+      </div>
+      <div class="content">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
@@ -21,9 +58,16 @@ nuxtApp.hook("page:finish", () => {
 
   > .page-container {
     font-size: $font-sm;
-    padding: 16px;
-    overflow-y: scroll;
-    height: calc(100% - #{$header-height});
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+    height: 100%;
+  }
+
+  > .page-container > .slider-show,
+  > .page-container > .content {
+    flex: 1;
   }
 }
 </style>
--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -1,28 +1,28 @@
 <script lang="ts" setup>
-import {signin } from '@/models/auth'
+import { signin } from "@/models/auth";
 definePageMeta({
   layout: "auth",
 });
-const form = ref()
-const username = ref('')
-const password = ref('')
+const form = ref();
+const username = ref("");
+const password = ref("");
 
-const onSubmit = async ()=>{
-  if(!form) return
-  const res = await signin(username.value, password.value)
-  console.log(res)
-}
+const onSubmit = async () => {
+  if (!form) return;
+  const res = await signin(username.value, password.value);
+  console.log(res);
+};
 
-const requiredName = (v: any) => !!v || `Full name is required`
+const requiredName = (v: any) => !!v || `Full name is required`;
 
-const requiredPassword = (v: any) => !!v || `Password is required`
-
+const requiredPassword = (v: any) => !!v || `Password is required`;
 </script>
 
 <template>
-  <v-sheet class="bg-deep-purple pa-12" rounded>
-    <v-card class="mx-auto px-6 py-8" max-width="344">
-      <v-form v-model="form" @submit.prevent="onSubmit">
+  <v-sheet class="login-form" rounded :height="'100%'">
+    <div class="wrapper">
+      <h1 class="title">TEST MANAGEMENT</h1>
+      <v-form v-model="form" @submit.prevent="onSubmit" class="form">
         <v-text-field
           v-model="username"
           :rules="[requiredName]"
@@ -53,6 +53,35 @@ const requiredPassword = (v: any) => !!v || `Password is required`
           Đăng nhập
         </v-btn>
       </v-form>
-    </v-card>
+    </div>
   </v-sheet>
 </template>
+
+<style lang="scss" scoped>
+.login-form {
+  background-color: $color-white;
+
+  > .wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  > .wrapper > .title {
+    margin-bottom: 56px;
+    color: $primary-color;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    width: 400px;
+    border: 1px solid $color-gray;
+    padding: 36px;
+    border-radius: 4px;
+  }
+}
+</style>
