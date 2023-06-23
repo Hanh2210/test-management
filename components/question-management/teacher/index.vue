fa8dbd6716a63d683769946d8fd9d9dd0eb86ad9
Hanh Nguyen <nguyen.thi.hong.hanh@azoom.jp>
2023-06-23 15:12:54 +0700

    update style

--- components/question-management/teacher/index.vue
+++ components/question-management/teacher/index.vue
@@ -196,7 +196,7 @@ const deleteQuestion = async (id: number) => {
 <style scoped lang="scss">
 .title {
   padding-bottom: 16px;
-  border-bottom: 1px solid $color-gray;
+  border-bottom: 1px solid #ccc;
   margin-bottom: 12px;
 }
 
